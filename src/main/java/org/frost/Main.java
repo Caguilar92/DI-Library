2c3a7a0e2f3a0f6be12daa0f4bd0a04b5ccb19bb
Candelario <candelarioa42@gmail.com>
2023-01-06 21:01:47 -0600

    "Initial Commit"

---
+++ src/main/java/org/frost/Main.java
@@ -0,0 +1,10 @@
+package org.frost;
+
+/**
+ * @author Candelario Aguilar Torres
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/frost/packagescanner/Scanner.java
@@ -0,0 +1,8 @@
+package org.frost.packagescanner;
+
+/**
+ * @author Candelario Aguilar Torres
+ **/
+public class Scanner {
+
+}
